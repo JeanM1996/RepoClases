114f01fddd20677960b4d5b737e0aa4542ba3e3f
salas <salas@UPS-SCH-006>
2016-07-12 10:39:16 -0500

    Version DOS

---
+++ src/ec/edu/utpl/jpmosquera/APP/Model/Estudiante.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.utpl.jpmosquera.APP.Model;
+
+/**
+ *
+ * @author salas
+ */
+public class Estudiante {
+    private String nombre;
+    private String apellido;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    
+    
+          
+}
